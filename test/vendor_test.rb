5d40051cf800d3d6aef448f3b9c9311f558efecd
Jenny Branham <branhamjenny@gmail.com>
2021-01-20 10:09:30 -0600

    Add vendor attributes

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -8,4 +8,11 @@ class VendorTest < Minitest::Test
 
     assert_instance_of Vendor, vendor
   end
+
+  def test_it_has_readable_attributes
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    assert_equal 'Rocky Mountain Fresh', vendor.name
+    assert_equal Hash.new, vendor.inventory
+  end
 end
