78e66464b424514ef34fa7f2a27376e053b7f3d7
Jenny Branham <branhamjenny@gmail.com>
2021-01-20 10:07:41 -0600

    Create vendor test

---
+++ test/vendor_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/pride'
+require 'minitest/autorun'
+require './lib/vendor'
+
+class VendorTest < Minitest::Test
+  def test_it_exists
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    assert_instance_of Vendor, vendor
+  end
