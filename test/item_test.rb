cfbe05bcbc6806b4e662b0ba61feaed6a9b79099
Jenny Branham <branhamjenny@gmail.com>
2021-01-20 10:05:55 -0600

    Test for item readable attributes

--- test/item_test.rb
+++ test/item_test.rb
@@ -8,4 +8,11 @@ class ItemTest < Minitest::Test
 
     assert_instance_of Item, item
   end
+
+  def test_it_has_readable_attributes
+    item = Item.new({name: 'Peach', price: "$0.75"})
+
+    assert_equal 'Peach', item.name
+    assert_equal 0.75, item.price
+  end
 end
