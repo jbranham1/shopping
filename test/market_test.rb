2909ba533d6f0c21b1930de33137fab471c18be8
Jenny Branham <branhamjenny@gmail.com>
2021-01-20 11:55:02 -0600

    Added more mocks

--- test/market_test.rb
+++ test/market_test.rb
@@ -40,13 +40,13 @@ class MarketTest < Minitest::Test
     assert_equal ["Vendor 1", "Vendor 2"], market.vendor_names
   end
 
-  def test_vendor_that_sell
+  def test_vendor_that_sell?
     market = Market.new("South Pearl Street Farmers Market")
     vendor1 = Vendor.new("Rocky Mountain Fresh")
-    item1 = Item.new({name: 'Peach', price: "$0.75"})
-    item2 = Item.new({name: 'Tomato', price: "$0.50"})
-    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    item1 = mock
+    item2 = mock
+    item3 = mock
+    item4 = mock
     vendor1.stock(item1, 35)
     vendor1.stock(item2, 7)
     vendor2 = Vendor.new("Ba-Nom-a-Nom")
